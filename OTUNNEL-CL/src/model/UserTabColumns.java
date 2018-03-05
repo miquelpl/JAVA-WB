65a12a8cb94a2d432ad1efb3afa9eee060cff645
Miguel Parrilla <miquelpl@telefonica.net>
2018-03-05 16:03:24 +0100

    Add files via upload

--- OTUNNEL-CL/src/model/UserTabColumns.java
+++ OTUNNEL-CL/src/model/UserTabColumns.java
@@ -1,6 +1,10 @@
 package model;
 
-public class UserTabColumns {
+import java.io.Serializable;
+
+public class UserTabColumns implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 	
 	private String tableName;
 	private String columnName;
@@ -10,6 +14,59 @@ public class UserTabColumns {
 	private int dataScale;
 	private String nullable;
 	private int columnId;
-
-
+	
+	public UserTabColumns() {
+		super();
+	}
+	
+	public String getTableName() {
+		return tableName;
+	}
+	public void setTableName(String tableName) {
+		this.tableName = tableName;
+	}
+	public String getColumnName() {
+		return columnName;
+	}
+	public void setColumnName(String columnName) {
+		this.columnName = columnName;
+	}
+	public String getDataType() {
+		return dataType;
+	}
+	public void setDataType(String dataType) {
+		this.dataType = dataType;
+	}
+	public int getDataLength() {
+		return dataLength;
+	}
+	public void setDataLength(int dataLength) {
+		this.dataLength = dataLength;
+	}
+	public int getDataPrecision() {
+		return dataPrecision;
+	}
+	public void setDataPrecision(int dataPrecision) {
+		this.dataPrecision = dataPrecision;
+	}
+	public int getDataScale() {
+		return dataScale;
+	}
+	public void setDataScale(int dataScale) {
+		this.dataScale = dataScale;
+	}
+	public String getNullable() {
+		return nullable;
+	}
+	public void setNullable(String nullable) {
+		this.nullable = nullable;
+	}
+	public int getColumnId() {
+		return columnId;
+	}
+	public void setColumnId(int columnId) {
+		this.columnId = columnId;
+	}
+	
+	
 }
