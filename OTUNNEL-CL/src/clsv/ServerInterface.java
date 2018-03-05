f4365cbfca689ba9f367ed30f784f672eb4a65b0
Miguel Parrilla <miquelpl@telefonica.net>
2018-03-05 16:04:26 +0100

    Add files via upload

--- OTUNNEL-CL/src/clsv/ServerInterface.java
+++ OTUNNEL-CL/src/clsv/ServerInterface.java
@@ -4,9 +4,10 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.List;
 
+import model.UserTabColumns;
 import model.UserTables;
 
 public interface ServerInterface extends Remote {
-	List<String> getTabColumns(String table) throws RemoteException; 
+	List<UserTabColumns> getTabColumns(String table) throws RemoteException; 
 	List<UserTables> getTables() throws RemoteException; 
 }
