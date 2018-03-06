da38aeec99a30b20142fe638707bd36970d1cf87
Miguel Parrilla <miquelpl@telefonica.net>
2018-03-06 11:53:16 +0100

    Add files via upload

---
+++ OTUNNEL-CL/src/model/Countries.java
@@ -0,0 +1,26 @@
+package model;
+
+public class Countries {
+	private char countryId;
+	private String countryName;
+	private int regionId;
+	public char getCountryId() {
+		return countryId;
+	}
+	public void setCountryId(char countryId) {
+		this.countryId = countryId;
+	}
+	public String getCountryName() {
+		return countryName;
+	}
+	public void setCountryName(String countryName) {
+		this.countryName = countryName;
+	}
+	public int getRegionId() {
+		return regionId;
+	}
+	public void setRegionId(int regionId) {
+		this.regionId = regionId;
+	}
+
+}
